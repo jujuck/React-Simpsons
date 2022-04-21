31ae51fabe3491ea7e607108094fa2c43961ab74
Julien ichard WCS <julien.y.richard@gmail.com>
2022-04-21 11:00:22 +0200

    Ajout du bouton pour listeners sur Simpson Card

--- src/components/SimpsonCard.jsx
+++ src/components/SimpsonCard.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 
 const SimpsonCard = ({ simpson }) => {
+
   return (
     <div className="col-4">
       <div className="card border m-4 h-100">
 
         <h4>{simpson.name}</h4>
         <h6>{simpson.id}</h6>
-        <p><small>{simpson.normalized_name}</small></p>
+        <button
+          className="btn btn-success"
+          onClick={() => alert(`Hello World, I am ${simpson.normalized_name}`)}
+        >
+          CLick On {simpson.name}
+        </button>
       </div>
     </div>
   )
