68ffee7f5bd4d7a6d103879132970e190a721115
Julien ichard WCS <julien.y.richard@gmail.com>
2022-04-21 09:54:56 +0200

    Mise en place du template pour un simpson

--- src/components/SimpsonList.jsx
+++ src/components/SimpsonList.jsx
@@ -4,7 +4,16 @@ import simpsons from '../data/simspons';
 const SimpsonList = () => {
   console.log(simpsons)
   return (
-    <div>SimpsonList</div>
+    <div>
+      <h2>
+        SimpsonList
+      </h2>
+      <div>
+        <h4>{simpsons[0].name}</h4>
+        <h6>{simpsons[0].id}</h6>
+        <p><small>{simpsons[0].normalized_name}</small></p>
+      </div>
+    </div>
   )
 }
 
