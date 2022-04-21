e0e009b037a5fea0549d5a612fd5f096210f1147
Julien ichard WCS <julien.y.richard@gmail.com>
2022-04-21 09:41:07 +0200

    Ajout du composant simspon parents

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import SimpsonList from './components/SimpsonList'
 
 function App() {
 
   return (
     <div className="App">
       <h1>Les Simpsons</h1>
+      <SimpsonList />
     </div>
   )
 }
---
+++ src/components/SimpsonList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SimpsonList = () => {
+  return (
+    <div>SimpsonList</div>
+  )
+}
+
+export default SimpsonList;
