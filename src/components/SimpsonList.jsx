9a50f0409432dd4e1d893ebaa89a023ac39b2c6d
Julien ichard WCS <julien.y.richard@gmail.com>
2022-04-21 10:21:00 +0200

    Ajout de la boucle pour affichage des 10 premiers résultats

--- src/components/SimpsonList.jsx
+++ src/components/SimpsonList.jsx
@@ -4,12 +4,16 @@ import SimpsonCard from './SimpsonCard';
 
 const SimpsonList = () => {
   console.log(simpsons)
+
   return (
     <div>
       <h2>
         SimpsonList
       </h2>
-      <SimpsonCard simpson={simpsons[1]} />
+      {simpsons.map((simpson, index) => {
+        if (index < 10) return <SimpsonCard simpson={simpson} />
+        return ""
+      })}
     </div>
   )
 }
